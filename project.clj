75ecf529e8f5dcb846f0ca95ab8a13a0302dbff2
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2017-05-15 23:13:37 +0100

    Added pre & post with clojure.spec

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU General Public License (GPL) version 3"
             :url "https://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"] [clj-time "0.13.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [clj-time "0.13.0"]]
   :main ^:skip-aot filldaysn.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/filldaysn/core.clj
+++ src/filldaysn/core.clj
@@ -1,6 +1,7 @@
 (ns filldaysn.core
   (:require [clj-time.core :as t]
-            [filldaysn.convert-utils :as conv])
+            [filldaysn.convert-utils :as conv]
+            [clojure.spec.alpha :as s])
   (:gen-class))
 
 (def one-day-adder (t/days 1))
@@ -26,10 +27,16 @@
        (cons (unavailable day)
              (next-day-lazy nextDay allDays))))))
 
+(s/def ::dates-seq (s/coll-of #(instance? org.joda.time.DateTime %) :distinct true))
+(s/def ::enhanced-date (s/keys :req-un [::date ::available]))
+(s/def ::enhanced-dates-seq (s/every ::enhanced-date))
+
 (defn next-day
   [[firstDay & _ :as all]]
+  {:pre [(s/valid? ::dates-seq all)]
+   :post [(s/valid? ::enhanced-dates-seq %)]}
   (if (nil? firstDay)
-    '()
+    (empty all)
     (next-day-lazy firstDay all)))
 
 (defn -main
